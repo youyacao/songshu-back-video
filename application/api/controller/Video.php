021d2e159fd23193c35986fa689529e786bafb07
bigniu <201309512@qq.com>
2019-09-29 18:05:07 +0800

    add databases

--- application/api/controller/Video.php
+++ application/api/controller/Video.php
@@ -4,12 +4,18 @@
 namespace app\api\controller;
 
 
+use think\Request;
+
 class Video
 {
-    public function getData(){
+    public function getData($request){
         return success("",["name"=>"test","url"=>"http://www.baidu.com"]);
     }
-    public function postData(){
+    public function postData(Request $request){
+        $title = input("title");
+        $user = session("user");
+        $video = input("video");
+
         return success("",["name"=>"test1","url"=>"http://www.baidu.com"]);
     }
 }
