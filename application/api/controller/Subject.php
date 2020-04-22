f742512fe72d4c6f6b3ddbf99027b7fdc16cfa73
zhantao <tzhang6@i9i8.com>
2020-04-22 14:49:45 +0800

    ok

--- application/api/controller/Subject.php
+++ application/api/controller/Subject.php
@@ -46,7 +46,7 @@ class Subject
         $total = Db("video_subject")
             ->where($where)
             ->count();
-        return success("获取成功", $list, $page, $total);
+        return success("获取成功", $list, 0, $total);
     }
 
     public function getResult(){
@@ -57,22 +57,22 @@ class Subject
         $page = input("page/i", 1) <= 1 ? 1 : input("page/i", 1);
         $pageSize = input("pageSize/i", 10) <= 10 ? 10 : input("pageSize/i", 10);
         $where = [];
-        $where['vsr.user_id'] = 1;
+        $where['vsr.uid'] = $user['id'];
         $list = Db("video_subject_result vsr")
             ->join("video v", "vsr.vid=v.id", "left")
             ->where($where)
             ->page($page, $pageSize)
             ->order("vsr.id desc")
-            ->field(['vsr.*','v.title','v.true_answer'])
+            ->field(['vsr.*','v.title'])
             ->select();
         foreach($list as &$val) {
             $val['results'] = json_decode($val['results'], true);
         }
-        $total = Db("video_subject_result")
+        $total = Db("video_subject_result vsr")
             ->join("video v", "vsr.vid=v.id", "left")
             ->where($where)
             ->count();
-        return success("获取成功", $list, 0, $total);
+        return success("获取成功", $list, $page, $total);
     }
 
     /**
@@ -87,7 +87,8 @@ class Subject
             return error("未登录");
         }
         $vid = intval(input('vid'));
-        $results = input('results');
+        $results = input('results/a');
+      
         $video = Db("video")->where(['id' => $vid])->find();
         if (empty($video)) {
             return error("视频不存在");
@@ -104,6 +105,7 @@ class Subject
             ->where($where)
             ->order("seconds asc")
             ->select();
+      
         $total = Db("video_subject")
             ->where($where)
             ->count();
@@ -123,39 +125,41 @@ class Subject
         }
         $data['vid'] = $vid;
         $data['uid'] = $user['id'];
+      
         $data['results'] = json_encode($results);
         $data['true_num'] = $true_num;
         $data['error_num'] = $total - $true_num;
         $data['all_gold'] = $all_gold;
         $data['update_time'] = time();
         $data['create_time'] = time();
-        $res_id = Db("video_subject_answer")->insertGetId($data);
+  		 
+        $res_id = Db("video_subject_result")->insertGetId($data);
         if (!$res_id) {
             Db::rollback();
             return error("答题提交失败");
         }
-
-        $allMoney = $user['money'] + $all_gold;
-        if (!Db('user')->where('id', $user['id'])->update(['money' => $allMoney])) {
-            Db::rollback();
-            return;
+		if ($all_gold) {
+            $allMoney = $user['money'] + $all_gold;
+            if (!Db('user')->where('id', $user['id'])->update(['money' => $allMoney])) {
+                Db::rollback();
+                 return error("更新金币错误");
+            }
+            //添加账变记灵
+            $data = array();
+            $data['user_id'] = $user['id'];
+            $data['num'] = $all_gold;
+            $data['before_money'] = $user['money'];
+            $data['after_money'] = $allMoney;
+            $data['info'] = '金币提现';
+            $data['data_id'] = $res_id;
+            $data['data_type'] = 'subject';
+            $data['created_at'] = date('Y-m-d H:i:s');
+            $res = Db('account_change')->insert($data);
+            if (!$res) {
+                Db::rollback();
+                return error("答题提交失败");
+            }
         }
-        //添加账变记灵
-        $data = array();
-        $data['user_id'] = $user['id'];
-        $data['num'] = $all_gold;
-        $data['before_money'] = $user['money'];
-        $data['after_money'] = $allMoney;
-        $data['info'] = '金币提现';
-        $data['data_id'] = $res_id;
-        $data['data_type'] = 'subject';
-        $data['created_at'] = date('Y-m-d H:i:s');
-        $res = Db('account_change')->insert($data);
-        if (!$res) {
-            Db::rollback();
-            return error("答题提交失败");
-        }
-
         // 提交事务
         Db::commit();
         return success("答题提交成功");
