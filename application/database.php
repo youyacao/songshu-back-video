2e3642f7348f1d13a11fa631ddf9d7d88494afa4
yinman.wu <wuyinman@126.com>
2021-01-10 20:59:09 +0800

    免费观看视频条数返回

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'mysql',
+    'hostname'        => 'localhost',
     // 数据库名
     'database'        => 'videofree',
     // 用户名
